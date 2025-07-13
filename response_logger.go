09a8119337380f157bb440f56a1aa1b1a2c4ef4c
Kyunghoon Kim <koorukuroo@gmail.com>
2025-07-13 16:49:57 +0900

    init

--- response_logger.go
+++ response_logger.go
@@ -2,8 +2,6 @@ package response_logger
 
 import (
     "bytes"
-    "context"
-    "io"
     "log"
     "net/http"
 
@@ -25,17 +23,13 @@ func (ResponseLogger) CaddyModule() caddy.ModuleInfo {
 }
 
 func (h ResponseLogger) ServeHTTP(w http.ResponseWriter, r *http.Request, next caddyhttp.Handler) error {
-    // 프록시 응답을 캡처할 기록기 생성
     rec := caddyhttp.NewResponseRecorder(w, nil, nil)
-    buf := new(bytes.Buffer)
-    rec.BufferWriter = io.MultiWriter(w, buf) // 복사 + 기록
-
     err := next.ServeHTTP(rec, r)
     if err != nil {
         return err
     }
 
-    log.Printf("[ResponseLogger] %s %s -> %d, Body: %s\n", r.Method, r.URL.Path, rec.Status(), buf.String())
-
+    body := rec.Body()
+    log.Printf("[ResponseLogger] %s %s -> %d\nBody:\n%s\n", r.Method, r.URL.Path, rec.Status(), string(body))
     return nil
 }
