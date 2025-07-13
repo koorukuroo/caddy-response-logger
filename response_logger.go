6e98448d952a4e0b4b21e58deb5add2a83752216
Kyunghoon Kim <koorukuroo@gmail.com>
2025-07-13 17:02:20 +0900

    init

--- response_logger.go
+++ response_logger.go
@@ -2,48 +2,49 @@ package response_logger
 
 import (
 	"bytes"
+	"context"
+	"io"
 	"log"
 	"net/http"
 
 	"github.com/caddyserver/caddy/v2"
-	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
 	"github.com/caddyserver/caddy/v2/modules/caddyhttp"
 )
 
 func init() {
 	caddy.RegisterModule(ResponseLogger{})
-	caddy.RegisterHandlerDirective("response_logger", parseCaddyfile)
 }
 
-// ResponseLogger is a simple middleware that logs the response body.
+// ResponseLogger implements a response handler module used within `handle_response`
 type ResponseLogger struct{}
 
+// CaddyModule returns the module information.
 func (ResponseLogger) CaddyModule() caddy.ModuleInfo {
 	return caddy.ModuleInfo{
-		ID:  "http.handlers.response_logger",
+		ID:  "http.handlers.copy_response.response_logger",
 		New: func() caddy.Module { return new(ResponseLogger) },
 	}
 }
 
-func (h ResponseLogger) ServeHTTP(w http.ResponseWriter, r *http.Request, next caddyhttp.Handler) error {
-	// Create a response recorder to capture status and body
-	rec := caddyhttp.NewResponseRecorder(w, nil, nil)
-	buf := new(bytes.Buffer)
-	rec.BufferWriter = buf
-
-	// Serve the next handler
-	err := next.ServeHTTP(rec, r)
-	if err != nil {
-		return err
-	}
-
-	// Log the status and body
-	log.Printf("[response_logger] %s %s -> %d\nBody:\n%s\n", r.Method, r.URL.Path, rec.Status(), buf.String())
-
+// Provision does nothing here but is required by interface
+func (ResponseLogger) Provision(_ caddy.Context) error {
 	return nil
 }
 
-// parseCaddyfile makes response_logger usable in Caddyfile.
-func parseCaddyfile(d *caddyfile.Dispenser) (caddyhttp.MiddlewareHandler, error) {
-	return ResponseLogger{}, nil
+// ResponseHandler interface: this is only allowed inside `handle_response` blocks
+func (ResponseLogger) ResponseHandler(next caddyhttp.ResponseHandler) caddyhttp.ResponseHandler {
+	return caddyhttp.ResponseHandlerFunc(func(w http.ResponseWriter, r *http.Request, resp *http.Response) error {
+		var buf bytes.Buffer
+		tee := io.TeeReader(resp.Body, &buf)
+
+		// Replace the response body with the tee reader
+		resp.Body = io.NopCloser(tee)
+
+		err := next.ServeHTTP(w, r, resp)
+
+		log.Printf("[response_logger] %s %s -> %d\nBody:\n%s\n",
+			r.Method, r.URL.Path, resp.StatusCode, buf.String())
+
+		return err
+	})
 }
