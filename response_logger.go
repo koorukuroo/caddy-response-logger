db51c5eb5b3c0c14f0941412a0f99f5ebb508cd6
Kyunghoon Kim <koorukuroo@gmail.com>
2025-07-13 17:03:13 +0900

    init

--- response_logger.go
+++ response_logger.go
@@ -2,7 +2,6 @@ package response_logger
 
 import (
 	"bytes"
-	"context"
 	"io"
 	"log"
 	"net/http"
@@ -26,23 +25,22 @@ func (ResponseLogger) CaddyModule() caddy.ModuleInfo {
 	}
 }
 
-// Provision does nothing here but is required by interface
+// Provision is a no-op
 func (ResponseLogger) Provision(_ caddy.Context) error {
 	return nil
 }
 
-// ResponseHandler interface: this is only allowed inside `handle_response` blocks
+// ResponseHandler processes the HTTP response
 func (ResponseLogger) ResponseHandler(next caddyhttp.ResponseHandler) caddyhttp.ResponseHandler {
 	return caddyhttp.ResponseHandlerFunc(func(w http.ResponseWriter, r *http.Request, resp *http.Response) error {
 		var buf bytes.Buffer
 		tee := io.TeeReader(resp.Body, &buf)
 
-		// Replace the response body with the tee reader
 		resp.Body = io.NopCloser(tee)
 
 		err := next.ServeHTTP(w, r, resp)
 
-		log.Printf("[response_logger] %s %s -> %d\nBody:\n%s\n",
+		log.Printf("[ResponseLogger] %s %s → %d\nBody:\n%s\n",
 			r.Method, r.URL.Path, resp.StatusCode, buf.String())
 
 		return err
